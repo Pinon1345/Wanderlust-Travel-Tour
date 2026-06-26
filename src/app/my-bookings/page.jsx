85dd91f3e9e65fc104fc5fafdf2b3c168fb54c62
Pinon1345 <pinonfurkan1@gmail.com>
2026-06-26 16:10:54 +0600

    implement booking card delete function

--- src/app/my-bookings/page.jsx
+++ src/app/my-bookings/page.jsx
@@ -8,6 +8,10 @@ import { MdOutlineAddLocation } from 'react-icons/md';
 import { RiDeleteBin6Line } from 'react-icons/ri';
 import { SlCalender } from 'react-icons/sl';
 import { LuEye } from "react-icons/lu";
+import { BookingCancelAlert } from '@/components/BookingCancelAlert';
+import Link from 'next/link';
+import { TfiFaceSad } from 'react-icons/tfi';
+import { IoChevronBackOutline } from 'react-icons/io5';
 
 const MyBookingsPage = async () => {
 
@@ -30,74 +34,101 @@ const MyBookingsPage = async () => {
             <div>
 
                 {
-                    bookings.map(booking =>
-                        <div
-                            key={booking._id}
-                            className='border-2 border-gray-200 rounded-lg shadow-lg mt-4 mb-8 p-4 flex flex-col md:flex-row items-center md:items-end justify-between gap-4'
-                        >
-                            {/* Left side */}
+                    bookings.length === 0
+                        ?
+                        (
+                            <div className="border-2 border-dashed border-gray-300 rounded-xl p-12 text-center shadow-sm mt-6 mb-6 flex flex-col items-center justify-center">
+                                <TfiFaceSad className='font-bold mb-6 w-15 h-15 text-purple-600'></TfiFaceSad>
+                                <h3 className="text-3xl font-bold text-gray-700">
+                                    No Bookings Yet
+                                </h3>
 
-                            <div className='flex flex-col items-center md:flex-row md:items-start py-1 gap-6'>
+                                <p className="text-gray-500 mt-3 mb-6">
+                                    Please add a booking from the Destination Details page.
+                                </p>
 
-                                <Image
-                                    className='w-90 h-65 object-cover rounded-2xl shadow-md shadow-slate-400'
-                                    src={booking.imageUrl}
-                                    alt={booking.destinationName}
-                                    width={800}
-                                    height={800}
-                                ></Image>
+                                <Link href="/destinations">
+                                    <Button
+                                        color="primary"
+                                        className="flex items-center gap-2 font-semibold pt-1 text-lg"
+                                    >
+                                        <IoChevronBackOutline></IoChevronBackOutline>
+                                        <h2>Explore Destinations</h2>
+                                    </Button>
+                                </Link>
+                            </div>
+                        )
+                        :
+                        (
 
-                                <div className='pt-2 pl-6'>
-                                    <Chip color="success" className='px-6 py-1 rounded-full text-sm mb-3'>
-                                        <CircleCheckFill width={12} />
-                                        <Chip.Label className='pt-0.5'>Confirmed</Chip.Label>
-                                    </Chip>
+                            bookings.map(booking =>
+                                <div
+                                    key={booking._id}
+                                    className='border-2 border-gray-200 rounded-lg shadow-lg mt-4 mb-8 p-4 flex flex-col md:flex-row items-center md:items-end justify-between gap-4'
+                                >
+                                    {/* Left side */}
 
-                                    <h2 className='text-3xl font-bold pt-3'>{booking.destinationName}</h2>
+                                    <div className='flex flex-col items-center md:flex-row md:items-start py-1 gap-6'>
 
-                                    <div className='flex items-center gap-1 pb-2'>
-                                        <SlCalender></SlCalender>
-                                        <p className='pt-1 text-lg font-semibold'>Departure: {new Date(booking.departureDate).toLocaleDateString(
-                                            "en-US", {
-                                            year: "numeric",
-                                            month: "long",
-                                            day: "numeric"
-                                        }
-                                        )}</p>
+                                        <Image
+                                            className='w-90 h-65 object-cover rounded-2xl shadow-md shadow-slate-400'
+                                            src={booking.imageUrl}
+                                            alt={booking.destinationName}
+                                            width={800}
+                                            height={800}
+                                        ></Image>
+
+                                        <div className='pt-2 pl-6'>
+                                            <Chip color="success" className='px-6 py-1 rounded-full text-sm mb-3'>
+                                                <CircleCheckFill width={12} />
+                                                <Chip.Label className='pt-0.5'>Confirmed</Chip.Label>
+                                            </Chip>
+
+                                            <h2 className='text-3xl font-bold pt-3'>{booking.destinationName}</h2>
+
+                                            <div className='flex items-center gap-1 pb-2'>
+                                                <SlCalender></SlCalender>
+                                                <p className='pt-1 text-lg font-semibold'>Departure: {new Date(booking.departureDate).toLocaleDateString(
+                                                    "en-US", {
+                                                    year: "numeric",
+                                                    month: "long",
+                                                    day: "numeric"
+                                                }
+                                                )}</p>
+                                            </div>
+
+                                            <p className='text-lg pb-2'>Booking ID: {booking._id}</p>
+
+                                            <h2 className='text-2xl md:text-3xl text-blue-700 font-bold mt-2'>${booking.price}</h2>
+                                        </div>
+
                                     </div>
 
-                                    <p className='text-lg pb-2'>Booking ID: b1</p>
+                                    {/* Right side */}
 
-                                    <h2 className='text-2xl md:text-3xl text-blue-600 font-bold mt-2'>${booking.price}</h2>
-                                </div>
+                                    <div className='flex justify-start pr-4 pb-4 pt-6 gap-3'>
 
-                            </div>
+                                        {/* Cancel */}
 
-                            {/* Right side */}
-
-                            <div className='flex justify-start pr-4 pb-4 pt-6 gap-3'>
-                                {/* Cancel */}
-                                <Button
-                                    variant='outline'
-                                    className="text-lg text-red-600 border-red-600 font-semibold hover:bg-red-500 hover:text-white px-5 py-4 rounded-lg"
-                                >
-                                    <RiDeleteBin6Line></RiDeleteBin6Line>
-                                    <p className='pt-1'>Cancel</p>
-                                </Button>
-
-                                {/* View */}
-                                <Button
-                                    variant='outline'
-                                    className="text-lg bg-[#15A1BF] text-white font-semibold hover:bg-cyan-800 hover:text-white px-6 py-4 rounded-lg"
-                                >
-                                    <LuEye></LuEye>
-                                    <p className='pt-1'>View</p>
-                                </Button>
-
-                            </div>
+                                        <BookingCancelAlert booking={booking}></BookingCancelAlert>
 
 
-                        </div>)
+                                        {/* View */}
+
+                                        <Button
+                                            variant='outline'
+                                            className="text-lg bg-[#15A1BF] text-white font-semibold hover:bg-cyan-800 hover:text-white px-6 py-4 rounded-lg"
+                                        >
+                                            <LuEye></LuEye>
+                                            <p className='pt-1'>View</p>
+                                        </Button>
+
+                                    </div>
+
+
+                                </div>)
+
+                        )
 
                 }
 
---
+++ src/components/BookingCancelAlert.jsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { AlertDialog, Button } from "@heroui/react";
+import { RiDeleteBin6Line } from "react-icons/ri";
+
+export function BookingCancelAlert({ booking }) {
+
+    const { destinationName, _id } = booking
+
+    const handleCancelBooking = async () => {
+        const res = await fetch(`http://localhost:5000/booking/${_id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-type': 'application/json'
+            }
+        })
+        const data = await res.json()
+        window.location.reload();
+        console.log(data)
+    }
+
+    return (
+        <AlertDialog>
+
+            <Button
+                variant='outline'
+                className="text-lg text-red-600 border-red-600 font-semibold hover:bg-red-500 hover:text-white px-5 py-4 rounded-lg"
+            >
+                <RiDeleteBin6Line></RiDeleteBin6Line>
+                <p className='pt-1'>Cancel</p>
+            </Button>
+
+            <AlertDialog.Backdrop>
+                <AlertDialog.Container>
+                    <AlertDialog.Dialog className="sm:max-w-100">
+                        <AlertDialog.CloseTrigger />
+                        <AlertDialog.Header>
+                            <AlertDialog.Icon status="danger" />
+                            <AlertDialog.Heading>Delete your booking permanently?</AlertDialog.Heading>
+                        </AlertDialog.Header>
+                        <AlertDialog.Body>
+                            <p>
+                                This will permanently delete <strong>{destinationName}</strong> and all of its
+                                data. This action cannot be undone.
+                            </p>
+                        </AlertDialog.Body>
+                        <AlertDialog.Footer>
+                            <Button slot="close" variant="tertiary" className="pt-1">
+                                Cancel
+                            </Button>
+                            <Button onClick={handleCancelBooking} slot="close" variant="danger" className="pt-1">
+                                Delete Booking
+                            </Button>
+                        </AlertDialog.Footer>
+                    </AlertDialog.Dialog>
+                </AlertDialog.Container>
+            </AlertDialog.Backdrop>
+        </AlertDialog>
+    );
+}
